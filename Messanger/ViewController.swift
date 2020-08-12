e636e86798f2a087336c71f51ed1feeabc3ed4db
nimamot <64922998+nimamot@users.noreply.github.com>
2020-08-12 15:27:59 -0700

    gettimg started

---
+++ Messanger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Messanger
+//
+//  Created by Nima on 8/12/20.
+//  Copyright © 2020 Nima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
