127dccb697c932458c2f593df89a6f04baaedd6e
nimamot <64922998+nimamot@users.noreply.github.com>
2020-08-12 15:47:04 -0700

    Rapp up getting started

---
+++ Messanger/Controllers/NewConversationViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NewConversationViewController.swift
+//  Messanger
+//
+//  Created by Nima on 8/12/20.
+//  Copyright © 2020 Nima. All rights reserved.
+//
+
+import UIKit
+
+class NewConversationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
